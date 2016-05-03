ac4d5247ac541a4b707812f885aa7fa3f2a13fcd
GiacomoSorbi <g.sorbi@gmail.com>
2016-05-03 13:15:17 +0100

    fixed problem in bundle.js

--- client/js/Products.js
+++ client/js/Products.js
@@ -10,42 +10,42 @@ module.exports = {
             sku: 'LN332',
             name: 'Isla Bra',
             price: 29,
-            image: bra1.jpg,
+            image: "bra1.jpg",
             inventory: 10
           },
           {
             sku: 'LN336',
             name: 'Nordic Rose Bra',
             price: 30,
-            image: bra2.jpg,
+            image: "bra2.jpg",
             inventory: 10
           },
           {
             sku: 'FY240',
             name: 'Zentangle Bra',
             price: 34,
-            image: bra3.jpg,
+            image: "bra3.jpg",
             inventory: 0
           },
           {
             sku: 'PN112',
             name: 'Clara Bra',
             price: 32,
-            image: bra4.jpg,
+            image: "bra4.jpg",
             inventory: 1
           },
           {
             sku: 'FY158',
             name: 'Deco Delight Bra',
             price: 34,
-            image: bra1.jpg,
+            image: "bra1.jpg",
             inventory: 1
           },
           {
             sku: 'LN328',
             name: 'Sienna Bra',
             price: 32,
-            image: bra2.jpg,
+            image: "bra2.jpg",
             inventory: 1
           }
         ]
--- client/js/bundle.js
+++ client/js/bundle.js
@@ -6,28 +6,57 @@ module.exports = {
     localStorage.setItem('product', JSON.stringify([
       {
         id: '0011001',
-        name: 'Scotch.io Signature Lager',
-        image: 'scotch-beer.png',
-        description: 'The finest lager money can buy. Hints of keyboard aerosol, with a whiff of iKlear wipes on the nose. If you pass out while drinking this beverage, Chris Sevilleja personally tucks you in.',
+        name: 'Bra collection',
+        image: 'bra1.jpg',
+        description: 'Best bra ever for every size and fasthion style; free delivery if you order 3 or more',
         variants: [
           {
-            sku: '123123',
-            type: '40oz Bottle',
-            price: 4.99,
+            sku: 'LN332',
+            name: 'Isla Bra',
+            type: 'Isla Bra',
+            price: 29,
+            image: "bra1.jpg",
+            inventory: 10
+          },
+          {
+            sku: 'LN336',
+            name: 'Nordic Rose Bra',
+            type: 'Nordic Rose Bra',
+            price: 30,
+            image: "bra2.jpg",
+            inventory: 10
+          },
+          {
+            sku: 'FY240',
+            name: 'Zentangle Bra',
+            type: 'Zentangle Bra',
+            price: 34,
+            image: "bra3.jpg",
+            inventory: 0
+          },
+          {
+            sku: 'PN112',
+            name: 'Clara Bra',
+            type: 'Clara Bra',
+            price: 32,
+            image: "bra4.jpg",
             inventory: 1
-
           },
           {
-            sku: '123124',
-            type: '6 Pack',
-            price: 12.99,
-            inventory: 5
+            sku: 'FY158',
+            name: 'Deco Delight Bra',
+            type: 'Deco Delight Bra',
+            price: 34,
+            image: "bra1.jpg",
+            inventory: 1
           },
           {
-            sku: '1231235',
-            type: '30 Pack',
-            price: 19.99,
-            inventory: 3
+            sku: 'LN328',
+            name: 'Sienna Bra',
+            type: 'Sienna Bra',
+            price: 32,
+            image: "bra2.jpg",
+            inventory: 1
           }
         ]
       }
